1cd5bf2ca8028076dc37cbd3c0e18611909614cc
Ryan Doubinin <ryan_doubinin@hotmail.com>
2020-11-29 13:57:24 -0700

    completed styling for shows and bio page

--- sprint-two/scripts/index.js
+++ sprint-two/scripts/index.js
@@ -3,16 +3,22 @@
 
 let comments = [
   {
-    fullName: "Ryan Doubinin",
-    profilePictureUrl: "./assets/images/Mohan-muruge3.png",
-    timeStamp: "11/28/2020",
-    comment: "JS Rules!",
-  },
-  {
     fullName: "Michael Lyons",
     profilePictureUrl: "./assets/images/Mohan-muruge3.png",
     timeStamp: "12/18/2018",
     comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+  },
+  {
+    fullName: "Gary Wong",
+    profilePictureUrl: "./assets/images/Mohan-muruge3.png",
+    timeStamp: "12/12/2018",
+    comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+  },
+  {
+    fullName: "Theodore Duncan",
+    profilePictureUrl: "./assets/images/Mohan-muruge3.png",
+    timeStamp: "11/15/2018",
+    comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
   },
 ];
 
@@ -50,7 +56,7 @@ function renderComments() {
 renderComments();
 
 function displayComment(comment) {
-  comments.push(comment);
+  comments.unshift(comment);
   renderComments();
 }
 
